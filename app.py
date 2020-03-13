be2a2fef72b87fea695dfc153192b169f0f20b78
Rivaldo Bizarrias <rivaldo.alvesb36@gmail.com>
2020-03-13 18:40:45 -0300

    Utilizando SQLAlchemy para realizar mapeamento.

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
-api = Api(app) 
+app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///banco.db' # Informa o tipo de banco que será utilizado
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Evita sobrecarregar a aplicação sem o rastreio de modificações 
+api = Api(app)
+
+@app.before_first_request
+def create_database():
+    db.create_all() # Cria automáticamente o banco e todas as tabelas
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__': #Quando for iniciado por linha de comando o serviço devera iniciar o servidor para executar a aplicação
+    from sql_alchemy import db
+    db.init_app(app)
     app.run(host='0.0.0.0', debug=True, port=8081)
--- models/hotel.py
+++ models/hotel.py
@@ -1,5 +1,14 @@
+from sql_alchemy import db
 
-class HotelModel:
+class HotelModel(db.Model):
+    __tablename__ = 'hotel'
+
+    hotel_id = db.Column(db.String, primary_key=True)
+    nome = db.Column(db.String(80))
+    estrelas = db.Column(db.Float(precision=1))
+    diaria = db.Column(db.Float(precision=2))
+    cidade = db.Column(db.String(40))
+
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id = hotel_id
         self.nome = nome
